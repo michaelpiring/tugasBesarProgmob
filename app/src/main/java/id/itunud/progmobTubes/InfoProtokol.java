f305ef4286ef2d6cf58a7620c0beaa29b46ff2fe
Michael Piring <piring.michael@gmail.com>
2021-04-20 06:49:07 +0800

    Info Karantina

---
+++ app/src/main/java/id/itunud/progmobTubes/InfoProtokol.java
@@ -0,0 +1,19 @@
+package id.itunud.progmobTubes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class InfoProtokol extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_protokol);
+
+    }
+}
--- app/src/main/java/id/itunud/progmobTubes/MainActivity.java
+++ app/src/main/java/id/itunud/progmobTubes/MainActivity.java
@@ -1,14 +1,29 @@
 package id.itunud.progmobTubes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardView = findViewById(R.id.infoprotokol);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), InfoProtokol.class);
+                startActivity(intent);
+            }
+        });
     }
 }
