c1fd5d5f13bb0fde8544455b591e3fb5af70b9c4
askarmuhammad <askarmuhammad666@gmail.com>
2021-05-18 15:54:16 +0800

    Info Karantina Final

--- app/src/main/java/id/itunud/progmobTubes/InfoProtokol.java
+++ app/src/main/java/id/itunud/progmobTubes/InfoProtokol.java
@@ -1,11 +1,8 @@
 package id.itunud.progmobTubes;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class InfoProtokol extends AppCompatActivity {
 
@@ -13,7 +10,7 @@ public class InfoProtokol extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info_protokol);
+        setContentView(R.layout.activity_info_karantina);
 
     }
 }
